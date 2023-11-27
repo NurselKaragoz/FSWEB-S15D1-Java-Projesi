524934813bc791864e41d0337d97b090f2dedc44
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-11-27 16:59:31 +0300

    scanner eklendi

--- src/com/workintech/arraylist/ShopList.java
+++ src/com/workintech/arraylist/ShopList.java
@@ -7,12 +7,27 @@ import java.util.ArrayList;
 public class ShopList {
     private static List<String> shopList=new ArrayList<>();
 
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String choosenNumber = scanner.nextLine();
+        String addItems= scanner.nextLine();
+        String removeItems= scanner.nextLine();
+        if(choosenNumber.equals("0")){
+            System.out.println("Uygulama durduruldu");
 
-        shopList.add("pepper");
+        }
+        else if(choosenNumber.equals("1")){
+            System.out.println("eklenecek malzemeyi yazınız"+ addItems);
+        }
+        else if(choosenNumber.equals("2")){
+            System.out.println("çıkarılacak malzemeyi yazınız"+ removeItems);
+        }
+
+
+        shopList.add("addItems");
+        shopList.add("removeItems");
         System.out.println(shopList);
         System.out.println("enter number:"+ choosenNumber);
 
